1a25bb059afc23b7bd00e5fa63b9b3f485f6cc3f
madt1m <blackjuniper@protonmail.com>
2018-01-09 16:04:03 +0100

    Main structure completed

--- comm.c
+++ comm.c
@@ -13,6 +13,7 @@
 #include "channel.h"
 #include "utilities.h"
 #define PASSWORD_LENGTH 8;
+#define MAX_READ 1024;
 
 int main(){
 	
@@ -21,6 +22,7 @@ int main(){
 		uint8_t button_pressed = 0b00010000;  // This stands for the end of input phase
 		int count = 0;  					  // Avoids password with length more than PASSWORD_LENGTH
 		channel conn_channel;
+		char buffer[MAX_READ];
 		char password[PASSWORD_LENGTH];
 		debug("Insert Password\n");
 		while(joystick_input != button_pressed && count < PASSWORD_LENGTH){
@@ -31,5 +33,22 @@ int main(){
 		debug("Password Inserted\n");
 		printLine(password);
 		conn_channel = openConnection();
+		if(conn_channel.code != SUCCESS){
+			printLine("[!!!] Channel connection has failed with code: ");
+			printLine(conn_channel.code);
+			printLine("\n");
+			//reset();
+		}
+		int bytes = writeTo(connection, password);
+		if(!bytes){
+			printLine("[!!!] Failure while writing to connection.");
+			//reset();
+		}
+		bytes = readFrom(connection, buffer);
+		if(!bytes){
+			printLine("[!!!] Failure while reading from connection.");
+			//reset();
+		}
+		printLine(buffer);		
 	}
 }
