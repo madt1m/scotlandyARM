f94dee67e55b1eb38bb35ac4bd8644c1b0b230ea
Lorenzo Santolini <>
2018-01-15 19:12:50 +0100

    dai cazzo

---
+++ UsefulFunctions.h
@@ -0,0 +1,4 @@
+// does an active open (tries to establish a connection between
+// 'MyIP:TCPLocalPort' and 'RemoteIP:TCPRemotePort')
+
+void TCPActiveOpen(void)
--- test.c
+++ test.c
@@ -13,18 +13,73 @@
 #include "timer.h"
 
 
+#include "easyweb.h"
+#include "type.h"
+#include "EMAC.h"         // Keil: *.c -> *.h    // ethernet packet driver
+#include "tcpip.h"        // Keil: *.c -> *.h    // easyWEB TCP/IP stack
+
+unsigned int pagecounter = 100;
+unsigned int adcValue    =   0;
+
+extern void TCPClockHandler(void);
+
+volatile DWORD TimeTick  = 0;
+
+void SysTick_Handler (void) {
+  TimeTick++;
+  if (TimeTick >= 20) {
+    TimeTick = 0;
+    LPC_GPIO2->FIOPIN ^= 1 << 0;
+    TCPClockHandler();  
+  }  
+}
+
+
 int main(){
 
+ 
+/*
 	uint8_t val;
 	uint8_t password[10];
-	int i = 0;
-	SystemInit(); 
+	int i = 0; 
 	joystickInit();
 	enable_timer(0);
+	SystemInit();
 	while(1) {
 		val = joystick_get_input();
 		delayMs(0, 5000);
 		password[i] = val;
 		i++;
 	}
+*/
+  SystemInit();                                      /* setup core clocks */
+  SysTick_Config(SystemFrequency/100);               /* Generate interrupt every 10 ms */
+
+  LPC_GPIO0->FIODIR   |= 1 << 21;					// ÉèÖÃLEDÓÐÐ§
+  LPC_GPIO0->FIOPIN	  |= 1 << 21;
+
+  LPC_GPIO2->FIODIR   |= 1 << 0;                    /* P2.0 defined as Output (LED) */
+
+  LPC_PINCON->PINSEL3 |=  (3ul<<30);                   /* P1.31 is AD0.5 */
+  LPC_SC->PCONP       |=  (1<<12);                   /* Enable power to ADC block */
+  LPC_ADC->ADCR        =  (1<< 5) |                  /* select AD0.5 pin */
+                          (4<< 8) |                  /* ADC clock is 25MHz/5 */
+                          (1<<21);                   /* enable ADC */ 
+  TCPLowLevelInit();
+
+   *(unsigned char *)RemoteIP = 192;               // inserisco l'ip del nostro server remoto
+  *((unsigned char *)RemoteIP + 1) = 168;          
+  *((unsigned char *)RemoteIP + 2) = 1;        
+  *((unsigned char *)RemoteIP + 3) = 4;
+
+  TCPActiveOpen();
+
+  while(1){
+  	DoNetworkStuff();
+  }
+
+
+
+
+
 }
