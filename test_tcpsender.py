7df3cf93667d1d4c63925834dc5025126b3487cd
Lorenzo Santolini <>
2018-01-19 16:48:14 +0100

    negotiation completed

--- test_tcpsender.py
+++ test_tcpsender.py
@@ -4,7 +4,7 @@ import sys
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	print('Connecting...')
-	s.connect(("172.20.238.243", 80))
+	s.connect(("192.168.1.8", 80))
 	s.send("PROVA".encode())
 
 
