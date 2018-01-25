72e299ece7c8664df1d9fae527d9d7ba4b96c1f9
Lorenzo Santolini <>
2018-01-25 18:36:29 +0100

    Server python almost ready

--- death_star.py
+++ death_star.py
@@ -10,41 +10,51 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind(('', PORT))
 	print('Socket created')
 	s.listen(1)
-	print('Waiting for connections')
-	conn, addr = s.accept()
-	print('Connected by')
-	print(addr)
-	data = conn.recv(8)
-	print(data.decode())
+	conn = None
 
-	if data == PASSWORD.strip():  
-		res = "200"
-		conn.send(res.encode())
-	else:
-	 	res = "401"
-	 	conn.send(res.encode())
-	while 1:
-		data = conn.recv(8)
-		if data == 2:
-			res = urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + 'addr' + '&position=true').read()
+	try:
+		print('Waiting for connections')
+		conn, addr = s.accept()
+		print('Connected by')
+		print(addr)
+		data = conn.recv(9).decode()
+		print(data)
+
+		if data == PASSWORD:  
+			res = "200"
 			conn.send(res.encode())
-		elif data[0] == 3:
-			if data[1] == 1 and data[2] == 2
-				res = ""
-				conn.send(res.encode())
-			if data[1] == 1 and data[2] == 2
-				res = ""
-				conn.send(res.encode())
-			if data[1] == 1 and data[2] == 2
-				res = ""
-				conn.send(res.encode())
-			if data[1] == 1 and data[2] == 2
-				res = ""
-				conn.send(res.encode())
-			if data[1] == 1 and data[2] == 2
-				res = ""
-				conn.send(res.encode())
+		else:
+		 	res = "401"
+		 	conn.send(res.encode())
+
+		while True:
 			
-		#conn.close()
+			data = conn.recv(1).decode()
+			if data == 2:
+				res = urllib.urlopen('http://api.hostip.info/get_html.php?ip=' + addr + '&position=true').read()
+				
+			elif data == 3:
+
+				ans = int(conn.recv(3).decode());
+				
+				if ans == 0:
+					res = "Darth Vader"
+
+				if ans == 1:
+					res = "Darth Maul"
+
+				if ans == 11:
+					res = "Count Duku"
+
+				if ans == 111:
+					res = "Darth Sidius"
+
+				if ans == 101:
+					res = "Kylo Ren"
+
+				
+			conn.send(res.encode())
+	except KeyboardInterrupt:
+			s.close()
 
 
