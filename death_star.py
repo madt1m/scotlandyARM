ac88c7b8b6c91bd7e8b05d922d8a2d2c37ede0c6
Lorenzo Santolini <>
2018-01-30 12:33:19 +0100

    death_star updated

--- death_star.py
+++ death_star.py
@@ -1,10 +1,14 @@
 import socket
 import sys
-import urllib
+import urllib.request
 
+if(len(sys.argv) != 2):
+	print("Specify port!")
+	exit(1)
 #HOST = "localhost"
-PORT = 12007
+PORT = int(sys.argv[1])
 PASSWORD = "lllduldul"
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.bind(('', PORT))
@@ -13,10 +17,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	conn = None
 
 	try:
+		
 		print('Waiting for connections')
 		conn, addr = s.accept()
 		print('Connected by')
 		print(addr)
+		
 		data = conn.recv(9).decode()
 		print(data)
 
@@ -26,35 +32,70 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		else:
 		 	res = "401"
 		 	conn.send(res.encode())
+		
 
 		while True:
 			
-			data = conn.recv(1).decode()
-			if data == 2:
-				req = urllib.request.Request('http://api.hostip.info/get_html.php?ip=' + addr + '&position=true', headers={'User-Agent': 'Mozilla/5.0'})
+			data = conn.recv(1)
+			print("ricevuto")
+			print(data)
+
+			if ord(data) == 1:
+				print("request 2 received")
+				'''
+				req = urllib.request.Request('http://api.hostip.info/get_html.php?ip=' + '172.20.238.136' + '&position=true', headers={'User-Agent': 'Mozilla/5.0'})
 				pos = urllib.request.urlopen(req).read().decode().splitlines()
-				res = pos[0] + " " +  pos[3] + " " + pos[4] + " " + post[5]
-			elif data == 3:
-
-				ans = int(conn.recv(3).decode());
+				'''
+				url = "http://api.hostip.info/get_html.php?ip=" + addr[0] + "&position=true"
+				print(url)
+				headers = {'User-Agent' : 'Mozilla/5.0'}
+				req = urllib.request.Request(url, headers = headers)
+				print(req)
+				pos = urllib.request.urlopen(req)
+				print(pos)
+				pos = pos.read()
+				print(pos)
+				pos = pos.decode()
+				print(pos)
+				pos = pos.splitlines()
+				print(pos)
+				res = pos[0] + " " +  pos[3] + " " + pos[4] + " " + pos[5]
 				
-				if ans == 0:
-					res = "Darth Vader"
-
-				if ans == 1:
-					res = "Darth Maul"
-
-				if ans == 11:
-					res = "Count Duku"
-
-				if ans == 111:
-					res = "Darth Sidius"
-
-				if ans == 101:
-					res = "Kylo Ren"
-
+				conn.send(res.encode())
+				print(res)
+				print("data sent back")
+			
+			elif ord(data) == 2:
+				print("request 3 received")
+				ans = conn.recv(3);
+				#ans = int.from_bytes(ans, byteorder='big', signed=False)
+				print(ans)
 				
-			conn.send(res.encode())
+				if ans == b'\x00\x00\x00':
+					res = "Darth Vader. That's impossible. I AM DARTH VADER! <force choke over the wire>"
+
+				elif ans == b'\x00\x01\x00':
+					res = "Darth Maul! How do you feel about those tiny little horns?"
+
+				elif ans == b'\x01\x01\x00':
+					res = "Count Duku! Definitely a man with his head right on shoulders."
+
+				elif ans == b'\x01\x00\x00':
+					res = "Darth Sidius. UNLIMITED POWER! THUNDER! NANANANAA NANANAA NAA"
+
+				elif ans == b'\x00\x00\x01':\
+					res = "...kylo ren. Feel sorry about that. You'll eventually get past adolescence, bru."
+				
+				else: 
+					res = "Yoda, the fake master...how did you get the device? YOU WILL BE EXECUTED!"
+
+
+				conn.send(res.encode())
+				print(res)
+				print("data sent back")
+			
+
+
 	except KeyboardInterrupt:
 			s.close()
 
